0153daaa52ec3d9e86e4a89d724250f8244c0946
AAFredsted <Andra_Fredsted@proton.me>
2024-03-03 21:41:14 +0100

    Exercises untill 4.7 solved

--- Exercises4.fsx
+++ Exercises4.fsx
@@ -10,7 +10,7 @@ let explode (s:string) =
 let rec explode2 (s:string) = 
     match s with 
     | "" -> []
-    | _ -> s.[0] :: explode2 (s.Remove(0, 1))
+    | _ -> s.[0] :: explode2 (s.Remove(0, 1));;
 
 
 //exercise 4.2
@@ -19,6 +19,57 @@ let (.+) (x1: string) (x2: char) =
     x1 + System.Char.ToString x2
 
 let implode (ls: char list) =
-    List.foldBack (fun elem acc -> acc .+ elem) ls ""
+    List.foldBack (fun elem acc -> acc .+ elem) ls "";;
 
 
+let implodeRev (ls: char list) = 
+    List.fold (fun (acc: string) (elem: char) -> acc .+ elem) "" ls;;
+
+
+
+
+
+//exercise 4.3
+//solution in one line
+let toUpper (s:string) = (explode s) |> List.map System.Char.ToUpper|> implode;;
+
+//using forward function composition
+let toUpperforward (s:string) = (explode >> (List.map System.Char.ToUpper) >> implode) s
+
+//using backward funtion composition
+let toUpperbackward (s:string) = (implode << (List.map System.Char.ToUpper) << explode) s
+
+//exercise 4.4
+
+let palindrome (s: string) = 
+                    if (explode >> (List.map System.Char.ToLower) >> implode) s = (explode >> (List.map System.Char.ToLower) >> implodeRev) s then true else false;;
+
+palindrome "Anna";;
+//exercise 4.5
+
+//implementation of ack-function
+let rec ack (m, n) = 
+        match (m, n) with
+        | (0, _) ->  n+1
+        | (m, 0) when m > 0 -> ack (m-1, 1)
+        | (m, n) when m > 0 && n > 0 -> ack( m-1, ack(m, n-1));;
+
+
+//exercise 4.6
+
+let time f =
+            let start = System.DateTime.Now in
+            let res = f () in
+            let finish = System.DateTime.Now in
+            (res, finish - start);
+
+let r = time (fun () -> ack (3, 11) );;
+let result, elapsedTime = r;;
+printfn "Result %A" result;;
+printfn "Elapsed Time %A" elapsedTime;;
+
+
+
+let timeArg1 f a = time (fun () -> ack a);;
+
+let b = timeArg1 ack (3, 11);;
